b08e14000190c24b3b061658417525458d724ce0
fonzhousefonzie <harth.fonz@gmail.com>
2022-07-02 19:02:58 +0200

    started

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TheHeader/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    TheHeader
+}
 }
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+
+:root{
+  --main-bg: #f0efef;
+  --main-color: #513e5e;
+  --green: #22ad96;
+  --white: #fefefe;
+  --light-blue: #b6b8d1;
+  --blue: #01afc8;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Roboto', Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: var(--main-color);
+}
+
+input[type="text"]{
+  border: none;
+  outline: none;
+  padding: 0.7rem 1rem;
+  border-radius: 0.3rem;
 }
 </style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,155 @@
+<template>
+    <header>
+        <div class="container d-flex justify-content-between align-items-center py-4">
+            <img src="../assets/img/dark-logo.png" alt="Max Coach Logo">
+            <div class="menu d-flex gap-1">
+                <ul class="menu-links d-flex list-unstyled gap-4">
+                    <li v-for="item in menu" :key="item.name">{{ item.name }} <i class="fa fa-solid fa-angle-down"></i>
+                        <ul class="dropdown list-unstyled">
+                            <li v-for="link in item.links" :key="link">{{ link }}</li>
+                        </ul>
+                    </li>
+                </ul>
+                <ul class="icons d-flex list-unstyled gap-4">
+                    <li><i class="fa fa-solid fa-cart-shopping"></i></li>
+                    <li><i class="fa-regular fa-circle-user me-3"></i></li>
+                </ul>
+                <div class="search">
+                    <input type="text" placeholder="Search...">
+                    <i class="fa fa-solid fa-magnifying-glass"></i>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            menu: {
+                home: {
+                    name: 'Home',
+                    links: {
+                        link1: 'link1',
+                        link2: 'link2',
+                        link3: 'link3'
+                    }
+                },
+                pages: {
+                    name: 'Pages',
+                    links: {
+                        link1: 'link1',
+                        link2: 'link2',
+                        link3: 'link3'
+                    }
+                },
+                courses: {
+                    name: 'Courses',
+                    links: {
+                        link1: 'link1',
+                        link2: 'link2',
+                        link3: 'link3'
+                    }
+                },
+                features: {
+                    name: 'Features',
+                    links: {
+                        link1: 'link1',
+                        link2: 'link2',
+                        link3: 'link3'
+                    }
+                },
+                blog: {
+                    name: 'Blog',
+                    links: {
+                        link1: 'link1',
+                        link2: 'link2',
+                        link3: 'link3'
+                    }
+                },
+                shop: {
+                    name: 'Shop',
+                    links: {
+                        link1: 'link1',
+                        link2: 'link2',
+                        link3: 'link3'
+                    }
+                }
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+header {
+    background: var(--main-bg);
+    color: var(--main-color);
+
+    .container {
+
+        img {
+            max-height: 2rem;
+        }
+
+        .menu-links,
+        .icons,
+        .search {
+            margin-bottom: 0;
+            align-items: center;
+
+            li {
+                padding: 0.5rem 0;
+                cursor: pointer;
+            }
+        }
+
+        .search {
+            position: relative;
+
+            i {
+                position: absolute;
+                right: 1rem;
+                top: 50%;
+                transform: translateY(-50%);
+                color: var(--green);
+            }
+        }
+
+        .menu-links li {
+            position: relative;
+
+            ul {
+                display: none;
+                position: absolute;
+                top: 100%;
+                right: 0;
+                background: var(--main-bg);
+                border: 1px solid var(--blue);
+                padding: 0.5rem 0;
+                color: var(--main-color);
+
+                li {
+                    padding: 0.5rem 2rem;
+                    cursor: pointer;
+
+                    &:hover {
+                        background: var(--white);
+                        color: var(--blue);
+                    }
+                }
+            }
+
+            &:hover {
+                color: var(--blue);
+                ul {
+                    display: block;
+                }
+            }
+        }
+    }
+
+
+}
+</style>
