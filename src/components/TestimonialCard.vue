ceb3c63ff31ee12db985d82ae19d264f70255e90
fonzhousefonzie <harth.fonz@gmail.com>
2022-07-04 10:00:35 +0200

    testimonials section

--- src/components/TestimonialCard.vue
+++ src/components/TestimonialCard.vue
@@ -1,56 +1,63 @@
 <template>
     <div class="testimonial-card">
         <div class="relative">
-            <img src="../assets/img/testimonial-avata-01.jpg" alt="">
-            <p class="main-text">I am free to learn at my own pace,
-                follow my own schedule and choose
-                the subject I like. Great study portal
-                for people like me.</p>
-            <h6>Mina Hollace</h6>
-            <p><span>/ </span>Freelancer</p>
+            <img :src="imgUrl" alt="">
+            <p class="main-text">{{text}}</p>
+            <h6>{{testimonialName}}</h6>
+            <p><span>/ </span>{{job}}</p>
         </div>
     </div>
 </template>
 
 <script>
 export default {
+    props:{
+        imgUrl: String,
+        text: String,
+        testimonialName: String,
+        job: String
+    }
 }
 </script>
 
 <style scoped lang="scss">
 .testimonial-card {
-    width: 32%;
-    padding: 0 1rem;
+    width: 30.4%;
+    flex-shrink: 0;
+    padding: 0 2rem;
     background: var(--white);
     border-radius: 0.5rem;
     box-shadow: 0 1rem 2rem rgba(0, 0, 0, 0.1);
-    height: 410px;
+    height: 350px;
 
     .relative {
         position: relative;
-        bottom: 3rem;
+        bottom: 1.5rem;
 
         img {
             position: relative;
             bottom: 3rem;
             border-radius: 50%;
-            width: 200px;
+            width: 150px;
         }
 
         .main-text {
             margin-top: -1.5rem;
-            font-size: 1.2rem;
+            font-size: 1rem;
+            font-weight: 500;
             color: var(--main-color);
         }
 
         h6 {
             text-transform: uppercase;
             font-weight: 600;
+            font-size: 1rem;
             padding-top: 2rem;
         }
 
         p {
             padding-top: 0.5rem;
+            font-size: 0.8rem;
         }
     }
 }
--- src/components/TestimonialsSection.vue
+++ src/components/TestimonialsSection.vue
@@ -1,48 +1,100 @@
 <template>
-  <div class="section py-5">
-    <div class="container pt-5">
-        <TitleAndSubtitle
-            :subtitle="'people are praising maxcoach'"
-            :title="'What make they'"
-            :title-last-word="'love us?'"
-        />
-        <div class="cards-container">
-            <TestimonialCard/>
-        </div>
-        <div class="call-to-action">
-            <div class="circle"></div>
-            <h5>Start today for getting <span>Online Certification</span></h5>
-            <h2>You can be your own guiding star with our help!</h2>
-            <button class="primary-btn large">Get started now</button>
+    <div class="section py-5">
+        <div class="container pt-5">
+            <TitleAndSubtitle :subtitle="'people are praising maxcoach'" :title="'What make they'"
+                :title-last-word="'love us?'" />
+            <div class="cards-container">
+                <TestimonialCard v-for="testimonial in testimonials" :key="testimonial.name"
+                    :img-url="testimonial.img"
+                    :text="testimonial.text"
+                    :testimonial-name="testimonial.name"
+                    :job="testimonial.job"
+                />
+            </div>
+            <div class="dots-container py-3 d-flex justify-content-center">
+                <div class="dot"></div>
+                <div class="dot"></div>
+                <div class="dot"></div>
+                <div class="dot"></div>
+            </div>
+            <div class="call-to-action">
+                <div class="circle"></div>
+                <h5>Start today for getting <span>Online Certification</span></h5>
+                <h2>You can be your own guiding star with our help!</h2>
+                <button class="primary-btn large">Get started now</button>
+            </div>
         </div>
     </div>
-  </div>
 </template>
 
 <script>
 import TitleAndSubtitle from './TitleAndSubtitle.vue';
 import TestimonialCard from './TestimonialCard.vue';
 export default {
+    data() {
+        return {
+            testimonials: [
+                {
+                    img: require('../assets/img/testimonial-avata-02.jpg'),
+                    text: "I am free to learn at my own pace, follow my own schedule and choose the subject I like.Great study portal for people like me.",
+                    name: "Mina Hollace",
+                    job: "Freelancer"
+                },
+                {
+                    img: require('../assets/img/testimonial-avata-04.jpg'),
+                    text: "MaxCoach is my best choice. Their tutors are smart and professional when dealing with students.",
+                    name: "Madley Pondor",
+                    job: "IT Specialist"
+                },
+                {
+                    img: require('../assets/img/testimonial-avata-01.jpg'),
+                    text: "I am happy with their arrangement of lessons and subjects. They reflect a scientific investigation.",
+                    name: "Luvic Dubble",
+                    job: "Private Tutor"
+                },
+                {
+                    img: require('../assets/img/testimonial-avata-03.jpg'),
+                    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus quia dicta corrupti nesciunt eum pariatur cupiditate optio quis.",
+                    name: "John Doe",
+                    job: "Web Developer"
+                },
+            ]
+        }
+    },
     components: { TitleAndSubtitle, TestimonialCard }
 }
 </script>
 
 <style scoped lang="scss">
-.section{
+.section {
     background: var(--main-bg-light);
-    .container{
-        .cards-container{
-            padding-top: 8rem;
+
+    .container {
+        .cards-container {
+            padding: 8rem 2rem 3rem;
             display: flex;
+            flex-wrap: nowrap;
+            gap: 3.5rem;
+            overflow-x: hidden;
         }
-        .call-to-action{
+
+        .dots-container{
+            .dot{
+                border: 4px solid var(--main-color);
+                border-radius: 50%;
+                margin: 0.7rem;
+            }
+        }
+
+        .call-to-action {
             padding: 2rem 0;
             position: relative;
             background: url('../assets/img/maxcoach-shape-01.png'), url('../assets/img/maxcoach-shape-02.png');
             background-repeat: no-repeat;
-            background-position: 1.5rem 6rem , right bottom;
+            background-position: 1.5rem 6rem, right bottom;
             background-size: 150px, 150px;
-            .circle{
+
+            .circle {
                 opacity: 0.2;
                 position: absolute;
                 border: 8px solid var(--tortora);
@@ -50,15 +102,18 @@ export default {
                 border-radius: 50%;
                 left: 0;
             }
-            h5{
+
+            h5 {
                 color: var(--main-color);
                 font-weight: 600;
-                span{
+
+                span {
                     color: var(--jungle-green);
                     font-weight: normal;
                 }
             }
-            h2{
+
+            h2 {
                 color: var(--main-color);
                 font-weight: 600;
                 padding-bottom: 1.5rem;
